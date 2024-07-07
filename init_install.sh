34b6d712f7a43b6ca47ac3e86313c18196843667
Ryuya Matsumoto <None>
2024-07-07 00:46:46 +0000

    update

--- init_install.sh
+++ init_install.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/googlechrom-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/googlechrom-keyring.gpg
 apt update
 apt install -y chromium chromium-driver google-chrome-stable
+pip3 install --user -r requirements.txt
--- pages/odds.py
+++ pages/odds.py
@@ -13,29 +13,20 @@ import time
 st.set_page_config(layout="wide")
 
 
-def detect_encoding(url):
-    response = requests.get(url)
-    result = chardet.detect(response.content)
-    return result['encoding']
-
-
 def main():
-    race_id = "202410030401"
-    base_url = "https://race.netkeiba.com/odds/index.html?type=b0&race_id=202410030401&rf=shutuba_submenu"
-    base_url = f"https://nar.netkeiba.com/odds/index.html?type=b1&race_id={race_id}"
 
+    # 対象のレースID
+    race_id_base = "2024100304"
+    race_num = st.selectbox("レース番号",[i for i in range(1,13)],format_func=lambda x: f"{x}R")
+    race_id = f"{race_id_base}{race_num:02}"
+    st.write(race_id)
 
-    encoding = detect_encoding(base_url)
-    st.write(encoding)
-    dfs = pd.read_html(base_url,encoding=encoding)
-    st.write(dfs)
-    
+    # オッズページのURL
+    b1_url = f"https://race.netkeiba.com/odds/index.html?type=b1&race_id={race_id}"
+    st.write(b1_url)
 
-def main2():
     # https://ohenziblog.com/streamlit_cloud_for_selenium/
     # Seleniumの設定
-    
-    # Using on Local
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
@@ -45,15 +36,9 @@ def main2():
     # 画面描画の待ち時間
     wait=WebDriverWait(driver,20)
     driver.implicitly_wait(30)
-    
-    # 対象のレースID
-    race_id = "202410030401"
-
-    # オッズページのURL
-    url = f"https://race.netkeiba.com/odds/index.html?type=b0&race_id={race_id}"
 
     # ページにアクセス
-    driver.get(url)
+    driver.get(b1_url)
     time.sleep(3)
     
     el= driver.find_element(By.CLASS_NAME, 'RaceOdds_HorseList_Table') #IDでテーブルを指定
@@ -82,4 +67,4 @@ def main2():
     
  
 if __name__ == "__main__":
-    main2()
+    main()
