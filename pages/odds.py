86a2b3c40ac01bdfbe366d0c6490dc95bbc4c2ba
Ryuya Matsumoto <akaneko1019@ryuya-matsumoto.com>
2024-07-07 00:08:55 +0000

    update

--- pages/odds.py
+++ pages/odds.py
@@ -40,7 +40,6 @@ def main2():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=1920,1200')
     options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                                 options=options)    
